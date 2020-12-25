d738173304b69c0c992504bef2b2025ce763b2e8
theforbiddenpool <contact@theforbiddenpool.com>
2020-12-25 06:38:14 +0000

    Fix white space

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const DEFAULT_API='https://run.mocky.io/v3/159b4afe-eba9-4560-84ac-27a9708e3315'
+const DEFAULT_API = 'https://run.mocky.io/v3/159b4afe-eba9-4560-84ac-27a9708e3315'
 const DEFAUL_LABEL = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
 const DEFAULT_VALUES = [null, null, null, null, null, null, null, null, null, null, null, null]
 
